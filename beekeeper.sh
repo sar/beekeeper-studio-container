2e70b17ca8ed051a51e0b6eaa781905c2b7ecaeb
Sar Malik <malik@sarthak.ca>
2021-09-23 11:55:22 -0400

    run:resolve security-opt flag default

--- beekeeper.sh
+++ beekeeper.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-x11docker --home=x11.beekeeper --hostnet x11.beekeeper:latest
+x11docker \
+	--home=x11.beekeeper \
+	--hostnet \
+	--share /data/workspace/aurora:ro \
+	-- --security-opt seccomp=unconfined --  \
+	x11.beekeeper:latest
 
 set +x;
